3dabe7e2edaeaee86dfb9db23e557db105bc7547
garsipal <48388897+garsipal@users.noreply.github.com>
2019-03-17 18:28:02 +0100

    Add files via upload

--- src/game/aiman.h
+++ src/game/aiman.h
@@ -115,8 +115,8 @@ namespace aiman
         ci->state.skill = skill <= 0 ? rnd(50) + 51 : clamp(skill, 1, 101);
         clients.add(ci);
         ci->state.lasttimeplayed = lastmillis;
-        if(!execfile("config/botname.cfg", false)) copystring(ci->name, "BOT", MAXNAMELEN+1);
-        else copystring(ci->name, newstring(ai::botnames[rnd(ai::botnames.length())]), MAXNAMELEN+1);
+        /*if(!execfile("config/botname.cfg", false))*/ copystring(ci->name, "BOT", MAXNAMELEN+1);
+        //else copystring(ci->name, newstring(ai::botnames[rnd(ai::botnames.length())]), MAXNAMELEN+1);
         ci->state.state = CS_DEAD;
         ci->team = team;
         ci->playermodel = rnd(128);

664d397ace18e78a8cba5ca72cb3b76db32daa13
garsipal <48388897+garsipal@users.noreply.github.com>
2022-08-02 22:05:51 +0200

    nerf rocket jump, increase pistol secondary push

--- src/game/weapon.h
+++ src/game/weapon.h
@@ -53,7 +53,7 @@ enum
 #define validsatk(n) ((n) >= ATK_TELEPORT && (n) < NUMATKS)
 
 #define MAXRAYS 20
-#define EXP_SELFPUSH 5.0f
+#define EXP_SELFPUSH 2.5f
 #define EXP_DISTSCALE 1.5f
 
 #define ALLY_DAMDIV 2 // divide damage dealt to self or allies
@@ -82,7 +82,7 @@ static const struct attackinfo
     { GUN_RAIL,   ACT_PRIMARY,  1200,  70, 70,   0, 0,    0, 40, 4000,  1, 110,  0,    0, 1, ANIM_SHOOT, ANIM_VWEP_SHOOT, ANIM_GUN_SHOOT, S_RAIL_A,        S_IMPACT_RAILGUN, S_HIT_RAILGUN },
     // pistol
     { GUN_PISTOL, ACT_PRIMARY,   300,   8,  5,   0, 0,    0,  0, 1000,  1, 200,  0,    0, 1, ANIM_SHOOT, ANIM_VWEP_SHOOT, ANIM_GUN_SHOOT, S_PISTOL1,       S_IMPACT_PULSE,   S_HIT_WEAPON  },
-    { GUN_PISTOL, ACT_SECONDARY, 600,  10,  0,   0, 5, 1000,  0, 2048,  1, 400,  8,  800, 2, ANIM_SHOOT, ANIM_VWEP_SHOOT, ANIM_GUN_SHOOT, S_PISTOL2,       S_IMPACT_PULSE,   S_HIT_WEAPON  },
+    { GUN_PISTOL, ACT_SECONDARY, 600,  10,  0,   0, 5, 1000,  0, 2048,  1, 500,  8,  800, 2, ANIM_SHOOT, ANIM_VWEP_SHOOT, ANIM_GUN_SHOOT, S_PISTOL2,       S_IMPACT_PULSE,   S_HIT_WEAPON  },
     // instagib
     { GUN_INSTA,  ACT_PRIMARY,  1200, 150,  0,   0, 0,    0, 60, 4000,  1,  30,  0,    0, 0, ANIM_SHOOT, ANIM_VWEP_SHOOT, ANIM_GUN_SHOOT, S_RAIL_INSTAGIB, S_IMPACT_RAILGUN, S_HIT_WEAPON  },
     // zombie

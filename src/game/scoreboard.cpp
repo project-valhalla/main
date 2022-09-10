1bf588af147407a9b09bedb7cc2efd89c80e4ebf
garsipal <48388897+garsipal@users.noreply.github.com>
2022-09-10 23:56:13 +0200

    add privilege colors back

--- src/game/scoreboard.cpp
+++ src/game/scoreboard.cpp
@@ -134,9 +134,12 @@ namespace game
         gameent *d = getclient(*cn);
         if(d)
         {
-            int status = 0xFFFFFF;
-            if (d->state == CS_DEAD || d->queue) status = 0x606060;
-            else if(d->state == CS_ALIVE && (d->juggernaut || d->zombie)) status = 0xFF6060;
+            int status = d->state!=CS_DEAD ? 0xFFFFFF : 0x606060;
+            if(d->privilege)
+            {
+                status = d->privilege>=PRIV_ADMIN ? 0xFF8000 : 0x40FF80;
+                if(d->state==CS_DEAD) status = (status>>1)&0x7F7F7F;
+            }
             intret(status);
         }
     });

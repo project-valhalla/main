52658ba360ac73479aed5d4f6feb95581422ed45
garsipal <48388897+garsipal@users.noreply.github.com>
2022-09-17 16:43:28 +0200

    update binaries path in `server.bat`

--- server.bat
+++ server.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-set TESS_BIN=bin
+set TESS_BIN=bin\bin32
 
 IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-    set TESS_BIN=bin64
+    set TESS_BIN=bin\bin64
 )
 IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
-    set TESS_BIN=bin64
+    set TESS_BIN=bin\bin64
 )
 
 start %TESS_BIN%\tesseract.exe "-u$HOME\My Games\ValhallaProject" -gserver-log.txt -d %*

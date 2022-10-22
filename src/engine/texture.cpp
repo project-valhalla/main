b151ac3cf0e130e21b8cfb98eebb433dda06256e
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2022-10-22 23:03:10 +0300

    increase texcompress limit to 4K
    also ensure it will trigger above that threshold, not on the mark -- having to use off-by-one values was dumb.

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -686,7 +686,7 @@ VAR(hwvtexunits, 1, 0, 0);
 VARFP(maxtexsize, 0, 0, 1<<12, initwarning("texture quality", INIT_LOAD));
 VARFP(reducefilter, 0, 1, 1, initwarning("texture quality", INIT_LOAD));
 VARFP(texreduce, 0, 0, 12, initwarning("texture quality", INIT_LOAD));
-VARFP(texcompress, 0, 1536, 1<<12, initwarning("texture quality", INIT_LOAD));
+VARFP(texcompress, 0, 4096, 1<<13, initwarning("texture quality", INIT_LOAD));
 VARFP(texcompressquality, -1, -1, 1, setuptexcompress());
 VARF(trilinear, 0, 1, 1, initwarning("texture filtering", INIT_LOAD));
 VARF(bilinear, 0, 1, 1, initwarning("texture filtering", INIT_LOAD));
@@ -709,7 +709,7 @@ void setuptexcompress()
 
 GLenum compressedformat(GLenum format, int w, int h, int force = 0)
 {
-    if(usetexcompress && texcompress && force >= 0 && (force || max(w, h) >= texcompress)) switch(format)
+    if(usetexcompress && texcompress && force >= 0 && (force || max(w, h) > texcompress)) switch(format)
     {
         case GL_R3_G3_B2:
         case GL_RGB5:

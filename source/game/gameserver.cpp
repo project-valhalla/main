cd5197ce575ad4bee1c70fb1ae959d23f1f768ac
garsipal <corra.patrick@gmail.com>
2024-02-18 17:11:06 +0100

    Update gameserver.cpp

--- source/game/gameserver.cpp
+++ source/game/gameserver.cpp
@@ -2733,7 +2733,7 @@ namespace server
             }
             if(!m_vampire(mutators) && actor->state.role == ROLE_JUGGERNAUT)
             {
-                actor->state.health = min(actor->state.health + 50, maximumhealth(actor->state.role == ROLE_ZOMBIE));
+                actor->state.health = min(actor->state.health + 50, maximumhealth(false));
                 sendf(-1, 1, "ri3", N_REGENERATE, actor->clientnum, actor->state.health);
             }
         }

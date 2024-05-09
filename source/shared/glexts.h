718792e2508f4d53b73caf0a7026223ff128295f
garsipal <corra.patrick@gmail.com>
2024-05-09 20:51:19 +0200

    enet 1.3.18
    Co-Authored-By: Lee Salzman <lsalzman@gmail.com>

--- source/shared/glexts.h
+++ source/shared/glexts.h
@@ -61,7 +61,7 @@
 #define GL_COMPRESSED_RGBA_S3TC_DXT1_EXT  0x83F1
 #define GL_COMPRESSED_RGBA_S3TC_DXT3_EXT  0x83F2
 #define GL_COMPRESSED_RGBA_S3TC_DXT5_EXT  0x83F3
-#endif 
+#endif
 
 #ifndef GL_EXT_timer_query
 #define GL_EXT_timer_query 1

ffe6a5e00562e58bc3f1a4ff422481e4defb72d4
garsipal <corra.patrick@gmail.com>
2024-10-29 19:57:20 +0100

    decrease default `deadpush` variable

--- source/game/gameclient.cpp
+++ source/game/gameclient.cpp
@@ -144,7 +144,7 @@ namespace game
     int sessionid = 0, mastermode = MM_OPEN, gamespeed = 100;
     string servdesc = "", servauth = "", connectpass = "";
 
-    VARP(deadpush, 1, 7, 20);
+    VARP(deadpush, 1, 5, 20);
 
     void switchname(const char* name)
     {

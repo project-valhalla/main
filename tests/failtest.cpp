3170852c8782556a35d0f72f3881ec7cabe48dbc
Sandu Liviu Catalin <slc.universe@gmail.com>
2026-01-02 17:41:37 +0200

    Implement CMake build scripts and github actions.
    Only Windows and Linux for now.
    Builds dependencies and links statically against them.
    Minimally intrusive approach. Only adds a few files and submodules.

---
+++ tests/failtest.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+static int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(0) == 1);
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}
+#if defined(__GNUC__) && !defined(_WIN32) // Doesn't crash for gcc/clang, but will with msvc
+TEST_CASE("Trigger ASan")
+{
+    int *array = new int[100];
+    delete [] array;
+    array[0] = 0; //boom
+    REQUIRE(false);
+}
+#endif
